c7b91fd1421a01cbc39045123f4ca6beb50c73ae
Daksha Patel <dakshapatel100@gmail.com>
2018-06-09 00:16:24 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -23,7 +23,7 @@ class Scraper
     profile_page = Nokogiri::HTML(open(profile_url))
     student = {}
     link =  profile_page.css(".social-icon-container").map
-    {el| el.attribute('href').value}}
+    {el| el.attribute('href').value}
     links.each do |link|
     if link.include?("linkedin")
       student[:linkedin] = link
