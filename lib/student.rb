08fcb650596b4c687a13636d1e7787ffecedb509
Daksha Patel <dakshapatel100@gmail.com>
2018-06-09 01:05:15 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,25 +1,24 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
   def initialize(student_hash)
-    
+
   end
 
   def self.create_from_collection(students_array)
-    #iterate over the array of hashes and create a new individual student using each hash. 
-    
+    #iterate over the array of hashes and create a new individual student using each hash.
+
   end
 
   def add_student_attributes(attributes_hash)
-    
+
   end
 
   def self.all
     @@all
-    
+
   end
 end
-
